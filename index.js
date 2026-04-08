79d6420198cd412861edb1ea9e7eb27e2fccffa3
jgmr2 <joseguadalupemr@protonmail.com>
2026-04-07 22:44:14 -0500

    CORS

--- index.js
+++ index.js
@@ -7,12 +7,28 @@ const swaggerSpec = require('./src/config/swagger');
 const app = express();
 
 connectDB(); 
+
+// CORS nativo para permitir cualquier origen, método y headers en toda la API.
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token');
+
+	if (req.method === 'OPTIONS') {
+		return res.sendStatus(204);
+	}
+
+	next();
+});
+
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/auth', require('./src/routes/authRoutes'));
 app.use('/api/event', require('./src/routes/eventRoutes'));
 app.use('/api/place', require('./src/routes/placeRoutes'));
+app.use('/api/places', require('./src/routes/placesRoutes'));
+app.use('/api/reservations', require('./src/routes/reservationsRoutes'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor en puerto:  ${PORT}`));
